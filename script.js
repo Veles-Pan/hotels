f46b9eaf314dd3f8d4cefc50a063be1f71d8de56
Veles-Pan <barbic-vika@ya.ru>
2021-02-03 02:42:19 +0300

    style: поправлена адаптивность

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let page = document.querySelector('.root')
+
 let button = document.querySelector('.header__popup-menu')
 
 let popupMenu = document.querySelector('.menu')
 
+let buttonFilter = document.querySelector('.show-filters__button')
+
+let filter = document.querySelector('.filters')
+
 function showPopup () {
     popupMenu.classList.toggle('menu_active')
+    page.classList.toggle('root_no-scroll')
+}
+
+function showFilters () {
+    filter.classList.toggle('filters_active')
+    page.classList.toggle('root_no-scroll')
 }
 
 button.addEventListener('click', showPopup); 
+
+buttonFilter.addEventListener('click', showFilters); 
